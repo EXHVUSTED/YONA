a2cfd2a463ecb8ac8bd1eaadc8b1bbac3bd65c1f
Nikita Gordeev <nikitaminecraft.90@gmail.com>
2025-09-07 20:54:11 +0200

    0.0.14

--- yona-project/src/components/LessonPlayer.tsx
+++ yona-project/src/components/LessonPlayer.tsx
@@ -4,7 +4,7 @@ import ReactPlayer from "react-player";
 export default function LessonPlayer({ url }: { url: string }) {
   return (
     <div className="w-full bg-black aspect-video">
-      <ReactPlayer src={url} controls width="100%" height="480px" />
+      <ReactPlayer src={url} controls width="100%" height="100%" />
     </div>
   );
 }
